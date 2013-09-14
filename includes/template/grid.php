4a45659b45d01e37a4b731a8555d2e31beb63d67
Aproducktion <Aproducktion@gmail.com>
2013-09-14 12:31:47 -0700

    [+] Includes Files / [+] Updated Styling

---
+++ includes/template/grid.php
@@ -0,0 +1,80 @@
+
+<h2>Grid System</h2>
+<div class="grid">
+	<div class="row">
+	    <div class="block col col-1">
+		   	<span class="noise center">1</span>	    
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="block col col-2">
+		   	<span class="noise center">2</span>	    
+	    </div>
+	    <div class="block col col-2">
+	    	<span class="noise center">2</span>	    
+	    </div>
+	</div>	
+	<div class="row">
+	    <div class="block col col-3">
+		   	<span class="noise center">3</span>	    
+	    </div>
+	    <div class="block col col-3">
+	    	<span class="noise center">3</span>	    
+	    </div>
+	    <div class="block col col-3">
+	    	<span class="noise center">3</span>
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="block col col-4">
+		   	<span class="noise center">4</span>	    
+	    </div>
+	    <div class="block col col-4">
+	    	<span class="noise center">4</span>	    
+	    </div>
+	    <div class="block col col-4">
+	    	<span class="noise center">4</span>
+	    </div>
+	    <div class="block col col-4">
+	    	<span class="noise center">4</span>
+	    </div>	    
+	</div>
+	<div class="row">
+	    <div class="block col col-5">
+		   	<span class="noise center">5</span>	    
+	    </div>
+	    <div class="block col col-5">
+	    	<span class="noise center">5</span>	    
+	    </div>
+	    <div class="block col col-5">
+	    	<span class="noise center">5</span>
+	    </div>
+	    <div class="block col col-5">
+	    	<span class="noise center">5</span>
+	    </div>
+	    <div class="block col col-5">	    
+			<span class="noise center">5</span>
+	    </div>	
+	</div>
+	<div class="row">
+	    <div class="block col col-6">
+		   	<span class="noise center">6</span>	    
+	    </div>
+	    <div class="block col col-6">
+	    	<span class="noise center">6</span>	    
+	    </div>
+	    <div class="block col col-6">
+	    	<span class="noise center">6</span>
+	    </div>
+	    <div class="block col col-6">
+	    	<span class="noise center">6</span>
+	    </div>	    
+	    <div class="block col col-6">	    
+			<span class="noise center">6</span>
+	    </div>
+	    <div class="block col col-6">	    
+			<span class="noise center">6</span>
+	    </div>		    	
+	</div>
+</div>	
+
